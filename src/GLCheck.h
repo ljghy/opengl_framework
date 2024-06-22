6bc1c04b4067462dab2792cc7b02b93949c8c382
ljghy <2436496086@qq.com>
2024-06-22 22:33:01 +0800

    update

--- src/GLCheck.h
+++ src/GLCheck.h
@@ -1,13 +1,12 @@
 #ifndef OPENGL_CHECK_H_
 #define OPENGL_CHECK_H_
 
-#include <glad/glad.h>
-#include <stdio.h>
-#include <assert.h>
-
 #if defined(NDEBUG) && !defined(GL_ENABLE_DEBUG)
 #define GL_CHECK(stmt) stmt
 #else
+#include <glad/glad.h>
+#include <stdio.h>
+#include <assert.h>
 #define GL_CHECK(stmt)                                                         \
     do                                                                         \
     {                                                                          \
